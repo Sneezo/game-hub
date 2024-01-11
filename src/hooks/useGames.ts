753553f949f4a9398b07cdd6ecb825077f33b5ab
Sneezo <vegard-blix.abrahamsen@hotmail.com>
2024-01-11 23:11:48 +0100

    Added loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,6 +24,7 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState(``);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -32,16 +33,18 @@ const useGames = () => {
       .get<FetchGamesResponse>(`/games`, { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
+        setLoading(false);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
