dfa3305916cfa9e3d2067a8e059ba6f286b5d356
Sneezo <vegard-blix.abrahamsen@hotmail.com>
2024-01-12 13:49:00 +0100

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = () => {
       <SimpleGrid
         width={"100%"}
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={10}
+        spacing={3}
         padding={10}
       >
         {isLoading &&
