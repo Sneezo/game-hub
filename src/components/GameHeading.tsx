83537c5f0fbee2e59e9dffcb9307b835b69ad2b0
Sneezo <vegard-blix.abrahamsen@hotmail.com>
2024-01-12 20:57:19 +0100

    Cleanup genrelist

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -10,7 +10,7 @@ const GameHeading = ({ gameQuery }: Props) => {
     gameQuery.genre?.name || ``
   } Games`;
   return (
-    <Heading marginY={2} as="h1">
+    <Heading marginBottom={2} as="h1">
       {heading}
     </Heading>
   );
